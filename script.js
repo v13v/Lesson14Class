4db8a8057e7fd7a76931fbc69ac7e1301896b2ec
Вадим <vadMoroz13@yandex.ru>
2020-06-25 22:05:53 +0300

    14часть1

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
 'use strict';
-let par = document.querySelector('.par')
-const DomElement = function(selector,height,width,bg,fontSize){
 
+const DomElement = function (selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
-    
-
-};
-debugger
-DomElement.prototype.creatSelectorElement = function(){
-    if(thes.selector.substr(0, 1) === '.'){
-      let div = document.createElement('div')
-      div.classList.add(this.selector)
-    } else if(this.selector.substr(0, 1) === '#'){
-        let pg = document.createElement('p')
-        pg.classList.add(this.selector)
-    }return
-
-div.style.cssText = `
-height : ${height}px;
-width : ${width}px;
-background : ${bg};
-fontSize : ${fontSize}px;
-`;
-
-pg.style.cssText = `
-height : ${height}px;
-width : ${width}px;
-background : ${bg};
-fontSize : ${fontSize}px;
-`;
-div.innerHTML = 'Любой текст 1';
-pg.innerHTML = 'Любой текст 2';
 };
 
+DomElement.prototype.creatSelectorElement = function () {
+    let elem;
+    if (this.selector.substr(0, 1) === '.') {
+        elem = document.createElement('div');
+        elem.setAttribute('class', this.selector);
+    } else if (this.selector.substr(0, 1) === '#') {
+        elem = document.createElement('p')
+        elem.setAttribute('id', this.selector);
+    }
+    elem.style.cssText = `
+    height : ${this.height}px;
+    width : ${this.width}px;
+    background : ${this.bg};
+    font-size : ${this.fontSize}%;
+    `;
+    elem.innerHTML = 'Любой текст 1';
+    document.body.insertAdjacentElement('beforebegin', elem);
+};
 
+let domElement = new DomElement('#money', 200, 400, 'tomato', 190);
 
-
-const domElement = new DomElement();
-
-domElement.creatSelectorElement('.blanc',30, 50, 'red', 10)
-
-console.log(domElement);
+domElement.creatSelectorElement();
